a83c25db58d92325c0cdb6646ee704b321643b74
lucas.costa <lucas.costa@SLT-002510>
2021-10-24 01:58:37 -0300

    Consertando mapa, add botoes de zoom, location atual

--- app/src/main/java/br/unifacs/AVN1/MainActivity.java
+++ app/src/main/java/br/unifacs/AVN1/MainActivity.java
@@ -1,26 +1,63 @@
 package br.unifacs.AVN1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button botaoConfig=(Button) findViewById(R.id.buttonConfig);
-        Button botaoSair=(Button) findViewById(R.id.buttonGNSS);
-        Button botaoSobre=(Button) findViewById(R.id.buttonSobre);
-        Button botaoNavegacao=(Button) findViewById(R.id.buttonNavegacao);
+        Button botaoConfig = (Button) findViewById(R.id.buttonConfig);
+        Button botaoSair = (Button) findViewById(R.id.buttonGNSS);
+        Button botaoSobre = (Button) findViewById(R.id.buttonSobre);
+        Button botaoNavegacao = (Button) findViewById(R.id.buttonNavegacao);
         botaoConfig.setOnClickListener(this);
         botaoSair.setOnClickListener(this);
         botaoSobre.setOnClickListener(this);
         botaoNavegacao.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+
     }
 
     @Override
@@ -33,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
                 break;
             case R.id.buttonNavegacao:
-                Intent i = new Intent(this, GoogleMapsActivity.class);
+                Intent i = new Intent(this, MapsActivity.class);
                 startActivity(i);
                 break;
             case R.id.buttonSobre:
@@ -41,4 +78,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
 }
--- app/src/main/java/br/unifacs/AVN1/MapsActivity.java
+++ app/src/main/java/br/unifacs/AVN1/MapsActivity.java
@@ -1,26 +1,48 @@
 package br.unifacs.AVN1;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import br.unifacs.AVN1.databinding.ActivityMapsBinding;
+//import br.unifacs.AVN1.databinding.ActivityMapsBinding;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    FusedLocationProviderClient client;
     private GoogleMap mMap;
     //private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        client = LocationServices.getFusedLocationProviderClient(this);
 
         //binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_maps);
@@ -40,13 +62,109 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        client.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @SuppressLint("MissingPermission")
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            Log.i("Teste", location.getLatitude() + " " + location.getLongitude());
+
+                            mMap.setMinZoomPreference(12.0f);
+                            mMap.setMaxZoomPreference(40.0f);
+
+                            mMap.setMyLocationEnabled(true);
+                            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                            mMap.getUiSettings().setZoomControlsEnabled(true);
+                            mMap.getUiSettings().isRotateGesturesEnabled();
+
+                            LatLng actualLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(actualLocation).title("Localização Atual"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(actualLocation, 15));
+
+
+                        } else {
+                            Log.i("Teste", "null");
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(8 * 1000);
+        locationRequest.setFastestInterval(2 * 1000);
+        locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(builder.build())
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i("Teste",
+                                locationSettingsResponse
+                                        .getLocationSettingsStates().isNetworkLocationPresent() + "");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ResolvableApiException) {
+                            try {
+                                ResolvableApiException resolvable = (ResolvableApiException) e;
+                                resolvable.startResolutionForResult(MapsActivity.this, 10);
+                            } catch (IntentSender.SendIntentException e1) {
+                            }
+                        }
+                    }
+                });
+
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult == null){
+                    Log.i("Teste 2", "local is null");
+                    return;
+                }
+
+                for(Location location : locationResult.getLocations()) {
+                    Log.i("Teste 2", location.getLatitude() + "");
+                }
+            }
+
+            @Override
+            public void onLocationAvailability(@NonNull LocationAvailability locationAvailability) {
+                Log.i("Teste", locationAvailability.isLocationAvailable() + "");
+            }
+        };
+        client.requestLocationUpdates(locationRequest, locationCallback, null);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
