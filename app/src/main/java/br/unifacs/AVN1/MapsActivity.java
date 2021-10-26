58f2f297102df0d3748f03e70734c3653e949818
Mello <lucasmellovieira99@gmail.com>
2021-10-25 22:08:01 -0300

    Colocando Longitude e Latitude na tela

--- app/src/main/java/br/unifacs/AVN1/MapsActivity.java
+++ app/src/main/java/br/unifacs/AVN1/MapsActivity.java
@@ -6,11 +6,16 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -31,12 +36,21 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 //import br.unifacs.AVN1.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     FusedLocationProviderClient client;
     private GoogleMap mMap;
+    private TextView textLatitude, textLongitude, textVelocidade;
+    private SharedPreferences sharedPrefs;
+    List<Location> local = new ArrayList<Location>();
+    private String cordenada, velocidade, orientacao, trafego, tipo;
+    public static final String SHARED_PREFES = "sharedPrefes";
     //private ActivityMapsBinding binding;
 
     @Override
@@ -51,6 +65,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        sharedPrefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        textLatitude = (TextView) findViewById(R.id.textLatitude);
+        textLongitude = (TextView) findViewById(R.id.textLongitude);
+        //textVelocidade = (TextView) findViewById(R.id.text_Velocidade);
     }
 
     /**
@@ -79,6 +98,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+        Location localizacaoAtual = locationManager.getLastKnownLocation(provider);
+
         client.getLastLocation()
                 .addOnSuccessListener(new OnSuccessListener<Location>() {
                     @SuppressLint("MissingPermission")
@@ -153,6 +177,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 for(Location location : locationResult.getLocations()) {
                     Log.i("Teste 2", location.getLatitude() + "");
+                    sharedPrefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+                    textLatitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+                    textLongitude.setText("Longitude: " +String.valueOf(location.getLongitude()));
+
                 }
             }
 
@@ -167,4 +195,115 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    public String coordenadas(double lat){
+        double valGrau, valMin, valSeg, auxSeg = 0;
+        String result = "";
+        String retorno = "";
+        String identificador = "COORDENADA";
+        String identificador2 = "COORDENADA2";
+        String identificador3 = "COORDENADA3";
+        double aux = lat;
+
+        lat = Math.abs(lat);
+
+        Boolean Coordenada2 = sharedPrefs.getBoolean("Coordenada_2", false);
+        Boolean Coordenada3 = sharedPrefs.getBoolean("Coordenada_3", false);
+
+        if(Coordenada2 == false && Coordenada3 == false){
+            //para grau
+            lat = Math.abs(lat);
+
+            valGrau = Math.floor(lat);
+            result = formatarFloat(valGrau, identificador) + "º";
+
+            auxSeg = Math.floor((lat - valGrau) * 60);
+            valMin = Math.floor((lat - valGrau) * 60) / 60;
+            result += formatarFloat(valMin, identificador2);
+
+            valSeg = (Math.floor((lat - valGrau - auxSeg / 60) * 3600 * 1000) / 1000) / 3600;
+            result += formatarFloat(valSeg, identificador3);
+        }
+
+        if(Coordenada2 == true){
+            //para grau e minutos
+            valGrau = Math.floor(lat);
+            result = formatarFloat(valGrau, identificador) + "º";
+
+            valMin = Math.floor((lat - valGrau) * 60);
+            result += formatarFloat(valMin, identificador) + ".";
+
+            valSeg = (Math.floor((lat - valGrau - valMin / 60) * 3600 * 1000) / 1000) / 3600;
+            result += formatarFloat(valSeg, identificador3) + "'";
+        }
+
+        if(Coordenada3 == true){
+            //para grau, minutos e segundos
+            valGrau = Math.floor(lat);
+            result = formatarFloat(valGrau, identificador) + "º";
+
+            valMin = Math.floor((lat - valGrau) * 60);
+            result += formatarFloat(valMin, identificador) + "'";
+
+            valSeg = Math.round((lat - valGrau - valMin / 60) * 3600 * 1000) / 1000;
+            result += formatarFloat(valSeg, identificador) + "''";
+        }
+        if(aux <= -0){
+            retorno = "-" + result;
+        }
+        else{
+            retorno = result;
+        }
+        return retorno;
+    }
+
+    public String formatarFloat(double numero, String identificador){
+        String retorno = "";
+        if(identificador.equals("VELOCIDADE")){
+            DecimalFormat formatter = new DecimalFormat("0.00");
+            try{
+                retorno = formatter.format(numero);
+            }catch(Exception ex){
+                System.err.println("Erro ao formatar numero: " + ex);
+            }
+        }
+        if(identificador.equals("COORDENADA")){
+            DecimalFormat formatter = new DecimalFormat("00");
+            try{
+                retorno = formatter.format(numero);
+            }catch(Exception ex){
+                System.err.println("Erro ao formatar numero: " + ex);
+            }
+        }
+        if(identificador.equals("COORDENADA2")){
+            DecimalFormat formatter = new DecimalFormat("#.00");
+            try{
+                retorno = formatter.format(numero);
+                retorno = retorno.replace(",", "");
+            }catch(Exception ex){
+                System.err.println("Erro ao formatar numero: " + ex);
+            }
+        }
+        if(identificador.equals("COORDENADA3")){
+            DecimalFormat formatter = new DecimalFormat("#.000");
+            try{
+                retorno = formatter.format(numero);
+                retorno = retorno.replace(",", "");
+            }catch(Exception ex){
+                System.err.println("Erro ao formatar numero: " + ex);
+            }
+        }
+        return retorno;
+    }
+
+    public void getShared(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFES, MODE_PRIVATE);
+
+        cordenada = sharedPreferences.getString("cordenadas", "unknown");
+        velocidade = sharedPreferences.getString("velocidade", "unknown");
+        orientacao = sharedPreferences.getString("orientacao ", "unknown");
+        trafego = sharedPreferences.getString("trafegon", "unknown");
+        tipo = sharedPreferences.getString("tipo", "unknown");
+    }
+
 }
